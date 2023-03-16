3c84b8074b141f95f9a4e5888b44a7a5cbd5ee40
Lyndon Panton <LyndonPanton@gmail.com>
2023-03-16 17:18:43 +0000

    Imported supplied starter files

---
+++ unity materials/ProgrammingAssignment3/Assets/Scripts/Listener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An event listener
+/// </summary>
+public class Listener : MonoBehaviour
+{
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	public void Start()
+	{
+		// add your code here
+	}
+	
+    /// <summary>
+    /// Handles the no argument event
+    /// </summary>
+    void HandleMessageEvent()
+    {
+        print("MessageEvent");
+    }
+
+    /// <summary>
+    /// Handles the one argument event
+    /// </summary>
+    /// <param name="number">number</number>
+    void HandleCountMessageEvent(int number)
+    {
+        print("CountMessageEvent: " + number);
+    }
+}
